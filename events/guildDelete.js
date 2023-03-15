9624e0d1ed8e3a4b2c08c2ee1b6d134d4cd36b03
ryzenix <61915235+ryzenix@users.noreply.github.com>
2023-03-15 22:46:53 +0700

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,6 +1,9 @@
 const emojisDb = require('../database/emojis.js');
 const dailyDb = require('../database/messagesDaily.js');
 const monthlyDb = require('../database/messagesWeekly.js');
+const dailyVoiceDb = require('../database/voiceDaily.js');
+const monthlyVoiceDb = require('../database/voiceWeekly.js');
+const memberVoiceDb = require('../database/memberVoice.js');
 
 module.exports = async(client, guild) => {
     await emojisDb.findOneAndDelete({
@@ -10,6 +13,15 @@ module.exports = async(client, guild) => {
         guildId: guild.id
     });
     await monthlyDb.findOneAndDelete({
+        guildId: guild.id
+    });
+    await dailyVoiceDb.findOneAndDelete({
+        guildId: guild.id
+    });
+    await monthlyVoiceDb.findOneAndDelete({
+        guildId: guild.id
+    });
+    await memberVoiceDb.findOneAndDelete({
         guildId: guild.id
     });
 }
